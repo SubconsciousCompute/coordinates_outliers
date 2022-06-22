00972d93f0e84d71fd7d8af8a229111d6ffdf4ce
sn99 <siddharthn.099@gmail.com>
2022-06-22 17:16:00 +0530

    add retain and push functionality

--- src/pointplane.rs
+++ src/pointplane.rs
@@ -1,3 +1,6 @@
+//! Our main entry point into the library. Maintains the frequency of points, edges, X and Y axis stats
+//! and capacity
+
 use crate::connection::Connection;
 use crate::graph::Graph;
 use crate::point::Point;
@@ -8,8 +11,8 @@ use std::collections::{HashMap, HashSet};
 /// A detailed struct containing summary of all the points, their frequencies and edge-weights
 #[derive(Debug, Clone)]
 pub struct PointPlane {
-    /// We hash the `Points`(upto 5 decimal places) by converting them into `String` due to float numerical limitations
-    points: HashMap<String, u32>,
+    /// We hash the `Points`(upto 3 decimal places) by converting them into `String` due to float numerical limitations
+    points: HashMap<String, usize>,
     graph: Graph,
     accumulate_xaxis: SimpleAccumulator,
     accumulate_yaxis: SimpleAccumulator,
@@ -18,10 +21,12 @@ pub struct PointPlane {
 
 impl PointPlane {
     /// Create new PointPlane
+    ///
+    /// If `i < j` ==> `points[i] // A -> points[j] // B` ==> `A->B`
     pub fn new(points: Vec<Point>, capacity: usize) -> Self {
         let mut graph = Graph(HashSet::<Connection>::with_capacity(100));
 
-        let mut frequencies: HashMap<String, u32> = HashMap::with_capacity(100);
+        let mut frequencies: HashMap<String, usize> = HashMap::with_capacity(100);
         let mut x = Vec::with_capacity(capacity);
         let mut y = Vec::with_capacity(capacity);
 
@@ -51,7 +56,7 @@ impl PointPlane {
     }
 
     /// Function to get the unique points in the plane
-    pub fn get_points(&self) -> &HashMap<String, u32> {
+    pub fn get_points(&self) -> &HashMap<String, usize> {
         &self.points
     }
 
@@ -76,8 +81,8 @@ impl PointPlane {
     }
 
     /// Function to get the frequency of a point in the plane
-    pub fn get_point_frequency(&self, point: &Point) -> u32 {
-        *self.points.get(&point.to_string()).unwrap_or(&0u32)
+    pub fn get_point_frequency(&self, point: &Point) -> usize {
+        *self.points.get(&point.to_string()).unwrap_or(&0usize)
     }
 
     /// Function to get the weight of an point in the plane (i.e. how many times it was used to traverse)
@@ -89,5 +94,34 @@ impl PointPlane {
             }
         }
         weight
+    }
+
+    /// We add new points into the existing point place again assuming the `Vec` of `Point`s
+    ///
+    /// If `i < j` ==> `points[i] // A -> points[j] // B` ==> `A->B`
+    pub fn push(&mut self, points: Vec<Point>) {
+        *self.points.entry(points[0].to_string()).or_insert(0) += 1;
+        self.accumulate_xaxis.push(points[0].x);
+        self.accumulate_yaxis.push(points[0].y);
+
+        for k in 1..points.len() {
+            self.accumulate_xaxis.push(points[k].x);
+            self.accumulate_yaxis.push(points[k].y);
+            self.graph.push(&Connection(
+                (points[k - 1].x).to_string() + " " + &*(points[k - 1].y).to_string(),
+                (points[k].x).to_string() + " " + &*(points[k].y).to_string(),
+                Cell::new(0),
+            ));
+
+            *self.points.entry(points[k].to_string()).or_insert(0) += 1;
+        }
+    }
+
+    /// Remove values based how many time it occurs
+    pub fn retain(&mut self, frequency: usize) {
+        self.points
+            .retain(|_, &mut point_frequency| point_frequency != frequency);
+
+        self.graph.0.retain(|k| k.2.get() != frequency);
     }
 }
